84140334448db104a3995c7b90261929c2fb27c5
unknown <mmu@ATCI-116.amccentre.local>
2015-09-24 15:35:39 +0200

    Opzet

---
+++ ValidGame/Assets/Editor/TestEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TestEditor : EditorWindow {
+ 
+    [MenuItem("AMC Toolkit/Card editor")]
+    static void Init()
+    {
+        TestEditor window = EditorWindow.GetWindow<TestEditor>();
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+       
+    }
+}
