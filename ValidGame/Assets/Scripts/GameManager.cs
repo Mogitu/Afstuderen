d9e62129fa4be156b59edb0b922ac78e46973396
Maikel <mogitu@msn.com>
2015-10-11 17:33:36 +0200

    *prototype

---
+++ ValidGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GameManager : NetworkManager{
+
+    // Use this for initialization
+    private GameObject mainCam;
+    private Animator camAnimator;
+    private bool gameStarted;
+    public GameObject gameMenu;
+    public GameObject mainMenu;
+
+	void Start () {
+        mainCam = Camera.main.gameObject;
+        camAnimator = mainCam.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HostGame()
+    {
+        StartServer();
+        mainMenu.SetActive(false);
+        gameMenu.SetActive(true);
+        RunAnimation();
+    }
+
+    public void JoinGame()
+    {
+        StartClient();
+        mainMenu.SetActive(false);
+        gameMenu.SetActive(true);
+        RunAnimation();
+    }
+
+    public void RunAnimation()
+    {
+        camAnimator.SetBool("GameStarted", true);
+    }
+}
