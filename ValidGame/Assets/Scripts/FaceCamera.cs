14b63e91fbf145b27aa591149da139b23549efd9
Maikel <mogitu@msn.com>
2015-10-13 20:02:01 +0200

    *outline added

---
+++ ValidGame/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+    // Use this for initialization
+    private Transform cameraTransform;
+	void Start () {
+        cameraTransform= Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraPosition = cameraTransform.position;
+
+        transform.LookAt(Camera.main.transform.position);
+	}
+}
