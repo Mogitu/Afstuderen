929416eaa50d46e8d72a6fb26c93064b3733e147
Maikel <mogitu@msn.com>
2015-10-25 23:58:06 +0100

    *all topics have correct(?) matchcodes(manually for now; will be automated by editor tool)

--- ValidGame/Assets/Scripts/SubtopicDescription.cs
+++ ValidGame/Assets/Scripts/SubtopicDescription.cs
@@ -2,23 +2,31 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class SubtopicDescription : MonoBehaviour {
+public class SubtopicDescription : MonoBehaviour
+{
 
-   
+
     public TextMesh descriptionTxt;
     private GameObject infoObject;
     // Use this for initialization
-    void Start () {
+
+    void Awake()
+    {
         infoObject = GameObject.Find("Infobar");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnMouseEnter()
-    {         
+    {
         Text infoText = infoObject.GetComponentInChildren<Text>();
         infoText.text = descriptionTxt.text;
     }
