8dba90c79d9551af3deeebece1c7ae983fba6c05
Maikel <mogitu@msn.com>
2016-01-14 16:56:48 +0100

    *created old and refactor folder for scripts.

---
+++ ValidGame/Assets/Scripts/Refactor/Cards/CardGrabber.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardGrabber  {
+
+
+    public void PickUpCard()
+    {
+
+    }
+
+
+    public void DropCard()
+    {
+
+    }
+
+
+    public void DragCard()
+    {
+
+    }
+}
