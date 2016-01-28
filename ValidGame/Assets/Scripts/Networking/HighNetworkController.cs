94d567863743115e58b44d71ed68f2df8c19375a
Maikel <mogitu@msn.com>
2016-01-28 18:39:37 +0100

    *server can talk with client, as it could, but now vice versa to!

--- ValidGame/Assets/Scripts/Networking/HighNetworkController.cs
+++ ValidGame/Assets/Scripts/Networking/HighNetworkController.cs
@@ -13,45 +13,52 @@ public class ChatMessage : MessageBase
 
 
 public class HighNetworkController : NetworkManager
-{    
+{
     public EventManager eventManager;
     private NetworkClient myClient;
 
     void Start()
     {
-        eventManager.AddListener(EVENT_TYPE.SEND, SendMsgs);      
-    }  
+        eventManager.AddListener(EVENT_TYPE.SEND, SendMsgs);
+    }
 
     public void SendMsgs(EVENT_TYPE Event_Type, Component Sender, object Param = null)
     {
         ChatMessage msgA = new ChatMessage();
         msgA.text = Param.ToString();
-        NetworkServer.SendToAll(MsgTypes.MSG_CHAT,msgA);
-        Debug.Log("Sent " + msgA.text);        
+        if (myClient != null)
+        {
+            myClient.Send(MsgTypes.MSG_CHAT, msgA);
+        }
+        NetworkServer.SendToAll(MsgTypes.MSG_CHAT, msgA);
+        Debug.Log("Sent " + msgA.text);
     }
 
     public void StartClient(string ip)
-    {           
-        myClient = new NetworkClient();        
+    {
+        myClient = new NetworkClient();
         myClient.RegisterHandler(MsgTypes.MSG_CHAT, OnChatMessageReceived);
-        myClient.Connect(ip, 7777);                            
+        myClient.Connect(ip, 7777);
     }
 
     void OnChatMessageReceived(NetworkMessage msg)
     {
-        ChatMessage msgA = msg.ReadMessage<ChatMessage>();        
-        eventManager.PostNotification(EVENT_TYPE.RECEIVE,this, msgA.text.ToString());
+        ChatMessage msgA = msg.ReadMessage<ChatMessage>();
+        Debug.Log("JAAAAA");
+        eventManager.PostNotification(EVENT_TYPE.RECEIVE, this, msgA.text.ToString());
     }
 
     public void MyStartHost()
     {
-        
-        StartHost();
-        client.RegisterHandler(MsgTypes.MSG_CHAT, OnChatMessageReceived);
+        NetworkServer.Listen(7777);
+        // myClient = ClientScene.ConnectLocalServer();
+        //StartHost();
+        NetworkServer.RegisterHandler(MsgTypes.MSG_CHAT, OnChatMessageReceived);
+        // myClient.RegisterHandler(MsgTypes.MSG_CHAT, OnChatMessageReceived);
         // StartServer();
         // myClient = new NetworkClient();        
         //myClient.RegisterHandler(MsgTypes.MSG_CHAT, OnChatMessageReceived);
         // myClient.Connect("127.0.0.1", 7777);                    
-    }   
+    }
 }
 
