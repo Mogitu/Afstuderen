7da41977e906f42b0aee0179ebf49d74677146a3
Maikel <mogitu@msn.com>
2016-03-30 11:11:01 +0200

    *optimized drawcalls for outlines

--- ValidGame/Assets/Scripts/Misc/ObjectOutline.cs
+++ ValidGame/Assets/Scripts/Misc/ObjectOutline.cs
@@ -8,37 +8,37 @@ public class ObjectOutline : MonoBehaviour
 {
     // Use this for initialization  
     //public Color HoverColor;
+    public Material HighLightMat;
     private Renderer ObjectRenderer;
-    //private Material DefaultMat;
-    //private Material HighLightMat;
-
-    private Shader DefaultShader;
-    private Shader OutlineShader;
+    private Material DefaultMat;
+    //private Shader DefaultShader;
+    //private Shader OutlineShader;
 
     void Start()
     {
-        DefaultShader = Shader.Find("Standard");
-        OutlineShader = Shader.Find("Toon/Basic Outline");
+        //DefaultShader = Shader.Find("Standard");
+        //OutlineShader = Shader.Find("Toon/Basic Outline");
         ObjectRenderer = GetComponent<Renderer>();
         //HighLightMat = Instantiate(ObjectRenderer.sharedMaterial);//Shared material because normal material will create extra instances "killing" performance.
-        //DefaultMat = ObjectRenderer.sharedMaterial;
+        DefaultMat = ObjectRenderer.sharedMaterial;
     }
 
     void OnMouseEnter()
     {
         if (enabled)
         {
-            //ObjectRenderer.material = HighLightMat;
+            ObjectRenderer.material = HighLightMat;
             //HighLightMat.color = HoverColor;
-            ObjectRenderer.material.shader = OutlineShader;
+            //ObjectRenderer.material.shader = OutlineShader;
         }
     }
 
     void OnMouseExit()
     {
         if (enabled)
-            // ObjectRenderer.material = DefaultMat;
-            ObjectRenderer.material.shader = DefaultShader;
+        {
+            ObjectRenderer.material = DefaultMat;
+            //ObjectRenderer.material.shader = DefaultShader;
+        }
     }
 }
-
