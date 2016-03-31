6d28ec0874597d487e128bf92b67758e71363f6b
Maikel <mogitu@msn.com>
2016-03-31 15:53:41 +0200

    *rotating fans added

---
+++ ValidGame/Assets/Scripts/Misc/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.forward * Speed);
+	}
+}
