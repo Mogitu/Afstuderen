40c4d93ba3677b8e55c5beb358ee64050890df29
Maikel <mogitu@msn.com>
2016-07-05 13:39:57 +0200

    *edits

--- ValidGame/Assets/Scripts/CoordinateScript.cs
+++ ValidGame/Assets/Scripts/CoordinateScript.cs
@@ -1,15 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CoordinateScript : MonoBehaviour {
 
+    private int ShowCoordinate;
+    private EventManager EventManager;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        EventManager = GameObject.FindObjectOfType<EventManager>();
+        EventManager.AddListener(GameEvents.UpdateSettings, OnUpdateSettings);
+        ShowCoordinate = PlayerPrefs.GetInt("ShowCoordinates", 0);
+        SetCoordinate();
+        if (ShowCoordinate == 1)
+        {
+            gameObject.SetActive(true);
+
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+    private void OnUpdateSettings(short eventType, Component sender, object param)
+    {
+        ShowCoordinate = PlayerPrefs.GetInt("ShowCoordinates", 0);
+        SetCoordinate();
+        if (ShowCoordinate == 1)
+        {
+            gameObject.SetActive(true);
+
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void HideCoordinate()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    private void DisplayCoordinate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void SetCoordinate()
+    {
+        
+        TextMesh txtMesh = GetComponent<TextMesh>();
+        GameObject root = transform.parent.parent.gameObject;
+        SubtopicMatcher match = root.GetComponentInChildren<SubtopicMatcher>();
+
+        if (match!=null)
+        {
+            txtMesh.text = match.MatchCode;
+        }
+        else
+        {
+            txtMesh.text = "?";
+        }
+    }	
 }
