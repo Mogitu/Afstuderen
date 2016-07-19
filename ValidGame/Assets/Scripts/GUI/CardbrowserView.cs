b3970e045b3e62b66d0c53de5987d6f2da638967
Maikel <mogitu@msn.com>
2016-07-19 09:11:01 +0200

    *scaling issues fixed completely

--- ValidGame/Assets/Scripts/GUI/CardbrowserView.cs
+++ ValidGame/Assets/Scripts/GUI/CardbrowserView.cs
@@ -15,38 +15,12 @@ public class CardbrowserView : View
     public GameObject CardPanelContent;
     public Image ExtraInfoPanelImage;
     private GuiPresenter GuiPresenter;
-    private float OffsetX = 80;
-    private float OffsetY = -90;
-
-    private float TargetResX = 1024;
-    private float TargetResY = 768;
-    private float CurrentResX ;
-    private float CurrentResY;
-
-    private float ResOffsetX;
-    private float ResOffsetY;
 
     void Awake()
-    {
-        CalculateOffsets();
+    {       
         GuiPresenter = GetPresenterType<GuiPresenter>();
         BrowsableCards = new Dictionary<string, GuiCard>();
-    }
-
-    private void CalculateOffsets()
-    {
-        CurrentResX = Screen.width;
-        CurrentResY = Screen.height;
-        ResOffsetX = CurrentResX / TargetResX;
-        ResOffsetY = CurrentResY / TargetResY;      
-    }
-
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        CalculateOffsets();
-
-    }
+    }    
 
     void Start()
     {
@@ -62,20 +36,16 @@ public class CardbrowserView : View
     /// TODO: get rid of hardcoded offsets. Also, perhaps, this is to much logic for in the view?
     /// </summary>
     private void PopulateContent()
-    {
-        //float widthScale = 1024 / Screen.width;
-        //float heightScale = 768 / Screen.height;
+    {        
         GuiCard[] cards = FindObjectsOfType<GuiCard>();
-        float offSetX = OffsetX;
-        float offSetY = OffsetY;
-        int col = 1;     
+       
         //add all cards
         if (GuiPresenter.GetTeamType == TeamType.ALL)
         {
             for (int i = 0; i < cards.Length; i++)
             {
                 //AddCard(cards[i], ref offSetX, ref offSetY, ref col);
-                AddCard(cards[i], ref offSetX, ref offSetY, ref col);
+                AddCard(cards[i]);
             }
         }
         //only add cards of selected teamtype
@@ -85,49 +55,18 @@ public class CardbrowserView : View
             {
                 if (GuiPresenter.GetTeamType == cards[i].TeamType)
                 {
-                    AddCard(cards[i], ref offSetX, ref offSetY, ref col);
+                    AddCard(cards[i]);
                 }
             }
-        }
-
-        //TODO: SLOPPY CALL; essentially populates the view twice.
-        RepopulateContent();
-    }
-
-    private void RepopulateContent()
-    {
-        float offSetX = OffsetX;
-        float offSetY = OffsetY;
-        int col = 1;
-        foreach (KeyValuePair<string, GuiCard> card in BrowsableCards)
-        {
-            GuiCard guiCard = card.Value;
-            guiCard.transform.SetParent(CardPanelContent.transform, false);
-            Vector3 newPos = guiCard.transform.parent.transform.position;
-            newPos.x += offSetX;
-            newPos.y += offSetY;
-            offSetX += (guiCard.GetComponent<RectTransform>().rect.width*1.1f)*ResOffsetX;
-            guiCard.transform.position = newPos;
-            col++;
-            if (col >= 5)
-            {
-                col = 1;
-                offSetY -= 200;
-                offSetX = OffsetX;
-            }
-        }
-    }
+        }       
+    }   
 
     //TODO: Split this up into smaller methods and get rid of hardcoded items
-    private void AddCard(GuiCard card, ref float offSetX, ref float offSetY, ref int col)
+    private void AddCard(GuiCard card)
     {
         GuiCard guiCard = card;
         guiCard.transform.SetParent(CardPanelContent.transform, false);
-        Vector3 newPos = guiCard.transform.parent.transform.position;
-        //newPos.x += offSetX*ResOffsetX;
-        //newPos.y += offSetY*ResOffsetY;
-        //offSetX += 150;
-      //  guiCard.transform.position = newPos;
+       
         Button objBtn = guiCard.GetComponent<Button>();
         objBtn.onClick.AddListener(() => { ClickedCard(objBtn.gameObject); });
 
@@ -140,15 +79,7 @@ public class CardbrowserView : View
         else
         {
             BrowsableCards.Add(guiCard.MatchCode, guiCard);
-        }
-        col++;
-
-        if (col >= 5)
-        {
-          //  col = 1;
-          //  offSetY -= 200;
-           // offSetX = OffsetX;
-        }
+        }      
     }
 
     void Update()
@@ -194,8 +125,7 @@ public class CardbrowserView : View
         GuiCard card = obj.GetComponent<GuiCard>();        
         GetPresenterType<GuiPresenter>().PickCard(card.MatchCode);
         BrowsableCards.Remove(card.MatchCode);
-        obj.SetActive(false);
-        RepopulateContent();
+        obj.SetActive(false);       
         GuiPresenter.EventManager.PostNotification(GameEvents.RestartTimer, this);
     }
 }
